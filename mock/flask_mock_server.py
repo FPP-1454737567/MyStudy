daab279d844188101d8a1ed45343cf4043920aa7
Panpan Fang <panpan.fang@shopee.com>
2024-02-26 11:57:29 +0800

    clear the repo

--- mock/flask_mock_server.py
+++ mock/flask_mock_server.py
@@ -1,26 +1,32 @@
+import json
+
 from flask import abort, jsonify, Flask, request, Response
 
 app = Flask(__name__)
 # 增加配置，支持中文显示
 app.config['JSON_AS_ASCII'] = False
 
-tasks = {
-    "header": {
-        "seq_id": "11234167868686876876",
-        "retcode": 0,
-        "rettype": "",
-        "message": ""
-    },
-    "can_reselect": True,
-    "failure_reason": "reselect_expired---"
-}
+# 方式1：直接定义mock的数据
+# tasks = {
+#     "header": {
+#         "seq_id": "11234167868686876876",
+#         "retcode": 0,
+#     },
+#     "can_reselect": True,
+#     "failure_reason": "reselect_expired---"
+# }
 
-@app.route('/mock_test', methods=['GET','POST'])
+# 方式2：从文件中读取，注意是load不是loads
+with open("./mock_data.json", "r") as data:
+    tasks = json.load(data)
+
+
+@app.route('/mock_test', methods=['GET', 'POST'])
 def get_task():
     return jsonify(tasks)
 
 
 if __name__ == '__main__':
-    app.run(host = '127.0.0.1',port = 6868,debug = True)
+    app.run(host='127.0.0.1', port=6868, debug=True)
 
-# url:127.0.0.1:6868/mock_test
+# curl 127.0.0.1:6868/mock_test
--- testApi/common/httpApi.py
+++ testApi/common/httpApi.py
@@ -12,7 +12,7 @@ import requests
 class HttpApi:
     def post(self):
         url = "https://http-gateway.spex.test.shopee.sg/sprpc/account.admin.change_user_status"
-        payload = json.dumps({
+        payload = {
                     "status": 0,
                     "reason": "test",
                     "ban_bank_accounts": False,
@@ -20,14 +20,14 @@ class HttpApi:
                     "userid": 1024730281,
                     "operator": "auto_data_script",
                     "region":"ID"
-                  })
+                  }
         header = {
             "x-sp-servicekey": "45318a86de2b7e59dec18a3bda5d5541",
             "x-sp-sdu": "account.admin.br.staging.master.default",
             "Content-Type": "application/json",
             "shopee-baggage": "CID=id"
         }
-        res = requests.post(url=url, data=payload, headers=header)
+        res = requests.post(url=url, data=json.dumps(payload), headers=header)
         print(res.status_code)
         print(res.text)
         print(type(res.text))
